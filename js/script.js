465734c223adb29aa6f9ce78e1c9c1779edd02a9
19alinka <91919141+19alinka@users.noreply.github.com>
2023-12-07 13:22:54 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.slider__items').slick({
+        arrows: false,
+        dots: true,
+        speed: 1000,
+        autoplay: true,
+        draggable: false,
+        waitForAnimate: false,
+       
+    });
+});
